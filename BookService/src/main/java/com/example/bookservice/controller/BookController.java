dd8b31e7ba5ba52a3917ee8c4525f3854777e89a
Naza1re <mcarim@mail.ru>
2023-11-18 12:59:41 +0300

    Add:book CRUD operations,library CRUD operations,end-point in library that provide finding all books that have no users.Add: routes in api-gateway

---
+++ BookService/src/main/java/com/example/bookservice/controller/BookController.java
@@ -0,0 +1,49 @@
+package com.example.bookservice.controller;
+
+import com.example.bookservice.exception.BookNotFoundException;
+import com.example.bookservice.model.Book;
+import com.example.bookservice.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.ws.rs.DELETE;
+import java.util.List;
+
+@RestController
+@RequestMapping("/books")
+public class BookController {
+    private final BookService bookService;
+    @Autowired
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+
+    @GetMapping("/all-books")
+    public ResponseEntity<List<Book>> getAllBooks(){
+        return bookService.getAllBooks();
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<Book> getBookById(@PathVariable Long id) throws BookNotFoundException {
+        return bookService.getBookById(id);
+    }
+    @GetMapping("/isbn/{isbn}")
+    public ResponseEntity<Book> getBookByISBN(@PathVariable String isbn) throws BookNotFoundException {
+        return bookService.getBookByISBN(isbn);
+    }
+    @PostMapping("/add-book")
+    public ResponseEntity<Book> addBook(@RequestBody Book book){
+        return bookService.addBook(book);
+    }
+    @PutMapping("/{id}/update")
+    public ResponseEntity<Book> updateBook(@PathVariable Long id,@RequestBody Book book) throws BookNotFoundException {
+        return bookService.updateBook(id,book);
+    }
+    @DeleteMapping("{id}/delete")
+    public HttpStatus deleteBook(@PathVariable Long id) throws BookNotFoundException {
+        return bookService.deleteBookById(id);
+    }
+
+}
---
+++ LibraryService/src/main/java/com/example/libraryservice/exception/LibraryNotFoundException.java
@@ -0,0 +1,7 @@
+package com.example.libraryservice.exception;
+
+public class LibraryNotFoundException extends Throwable {
+    public LibraryNotFoundException(String s) {
+        super(s);
+    }
+}
